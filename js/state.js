0e69c91eaa438e7e3e1c7e7f88920cc39afaef59
antoto2021 <90917299+antoto2021@users.noreply.github.com>
2026-01-18 12:22:37 +0100

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,10 @@
+// --- ÉTAT GLOBAL (VARIABLES) ---
+let firebaseInstance = null, myUid = null;
+let collection = [], contentMap = {}, customSections = [], friends = [], currentFriendItems = [];
+let isEditMode = false, currentPhotos = [], currentSectionImage = null;
+let activeUpdates = [{ icon: "🚀", title: "Mise à jour", desc: "Nouvelle version disponible." }];
+let currentSlide = 0;
+let comparatorChart = null; 
+let currentViewingFriendId = null;
+let currentCaliType = 'spot'; 
+let allCaliSpotsCache = [];
